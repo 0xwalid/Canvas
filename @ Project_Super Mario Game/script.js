7f3f55653fd9cbe27c01e10a6e444d6dfc602b7b
Petyo Mitkov <petyo.mitkov@gmail.com>
2016-11-27 23:53:31 +0200

    Creat Folder - Super Mario Geme

---
+++ @ Project_Super Mario Game/script.js
@@ -0,0 +1,16 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+function draw() {
+    ctx.beginPath();
+    ctx.fillRect(0,0,50,50);
+}
+draw();
+
+/*
+draw background
+draw mario
+move background and mario 
+
+
+ */
