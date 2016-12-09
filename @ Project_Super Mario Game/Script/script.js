f629da3aec12c5474b7f6bd1a5470ba6229bf246
Petyo Mitkov <petyo.mitkov@gmail.com>
2016-12-09 17:28:20 +0200

    Mario - Added Jumps and BrickCoin Objects

--- @ Project_Super Mario Game/Script/script.js
+++ @ Project_Super Mario Game/Script/script.js
@@ -16,13 +16,14 @@ let marioObj = {
     scaleRight: 1,
     jumpStart: false,
     onGround: true,
-    constantFloor: 448
+    constantFloor: 448,
+    jumpLimit: 220
 }
-let globalPosition = 200; 
+let globalPosition = 350; 
 let keysObj = {};
 let moveSpeed = 2;
 let isLoading = true;
-let audio = new Audio("Music/marioMusic.mp3");
+let audio = new Audio("Music/marioMusic.mp3"); //marioMusic.mp3
 audio.volume = 0.03;
 
 
@@ -33,9 +34,7 @@ window.addEventListener("keyup", eventHandler);
 
 
 //Start Loading Animation 
-/////////////////////////////////////////////////////////////////////
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 
 let loadingSign = "Loading";
@@ -116,9 +115,20 @@ let progress = 0;
     }
 
     
-/////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
 
+function controlsInstructions() {
+    ctx.font = "45px monospace";
+    ctx.fillStyle = "white";
+    ctx.fillText("You can Start to Play",80,250); 
+    ctx.font ="30px monospace";
+    ctx.fillText("Use Left, Right Arrow and Space.",80,300);  
+    loadingCounter++;
 
+    if (globalPosition > 405) {
+        loadingCounter = 2000;
+    }
+}
 
 function eventHandler(event) {
     if (event.type == "keydown") {
@@ -126,51 +136,44 @@ function eventHandler(event) {
     }
     if (event.type == "keyup") {
         delete keysObj[event.code];
-        //isMove = false; 
     }
-    //updateContlos();
 }
 
-function updateContlos() {
-    if (keysObj["ArrowRight"] || keysObj["ArrowLeft"]) {
+function updateContlos() { 
+    if (keysObj["ArrowRight"] || keysObj["ArrowLeft"]) {        
         isMove = true;
     } else {
         isMove = false;
     }
 
-
     if (keysObj["ArrowRight"] && marioObj.translateX < 2940) {         
         if (marioObj.x >= 580) {            
             marioObj.translateX += moveSpeed; 
-            globalPosition += moveSpeed;      
         } else if (marioObj.x < 580) {
-            marioObj.x += moveSpeed;
-            globalPosition += moveSpeed;
-        }  
+            marioObj.x += moveSpeed;   
+        } 
+        globalPosition += moveSpeed; 
         marioObj.scaleRight = 1;           
     } 
     if (keysObj["ArrowLeft"]) {       
         if (marioObj.translateX >= moveSpeed) {
             marioObj.translateX -= moveSpeed;
-            globalPosition -= moveSpeed;
         } else if (marioObj.x >= moveSpeed){
-             marioObj.x -= moveSpeed;
-             globalPosition -= moveSpeed;
+            marioObj.x -= moveSpeed;            
+        }
+        if (globalPosition > 0) {
+            globalPosition -= moveSpeed;
         }
         marioObj.scaleRight = -1; 
     } 
-
-    //under construction 
-    if (keysObj["Space"]) {   
+    if (keysObj["Space"]) {           
         if (marioObj.y == marioObj.constantFloor ) { 
-            marioObj.jumpStart = true;            
-        }
+            marioObj.jumpStart = true; 
+        }           
         if (marioObj.y < marioObj.constantFloor ) { 
             marioObj.onGround = false;
         }
     }
-    //mainGameLoop(); 
-    //draw();
 }
 
 //Background
@@ -179,77 +182,124 @@ function drawBackground(x,y){
         ctx.fillRect(0,0,50,50);
 }
 
-function marioStepsAndRotate(scaleRight,x,y) {
+function marioStepsAndRotate() {
     ctx.save();
-    if (scaleRight == -1) {
+    let x = marioObj.x;
+    if (marioObj.scaleRight == -1) {
         x += 68;
     }
-    ctx.translate(x,y);
-    ctx.scale(scaleRight,1)
-    if (globalPosition % 4 == 0 && isMove == true) {
+    ctx.translate(x, marioObj.y);
+    ctx.scale(marioObj.scaleRight,1)
+    if (marioObj.onGround == false) {
+        ctx.drawImage(marioImg1,0,0,34,34,0,0,(34*2),(34*2));
+    } else if (globalPosition % 4 == 0 && isMove == true) {
         ctx.drawImage(marioImg1,0,0,34,34,0,0,(34*2),(34*2));
     } else if (globalPosition % 2 == 0) {
         ctx.drawImage(marioImg,0,0,34,34,0,0,(34*2),(34*2));
     }    
+
     ctx.restore();
 }
 
-//under construction 
+let jumpUpOrDown = "up"; 
 function marioPhysics() {    
-    if (marioObj.jumpStart == true && keysObj["Space"] == true) {   //jump
-        marioObj.y -= 15;
-        if (marioObj.y <= 300) {
-            marioObj.jumpStart = false;
-        }
-    } else if (marioObj.onGround == false && marioObj.y < 448) {    //falling
-        marioObj.y += 10;          
-    } else if (marioObj.y >=448) {         // set mario on floor when falling
-        marioObj.y = marioObj.constantFloor;
-        marioObj.onGround = true; 
-    }         
+        if (marioObj.jumpStart == true && keysObj["Space"] == true && jumpUpOrDown == "Up") {   //jump
+            marioObj.y -= 7;
+            if (marioObj.y <= marioObj.jumpLimit) {
+                marioObj.jumpStart = false;
+            }
+        } else if (marioObj.onGround == false && marioObj.y < marioObj.constantFloor) {    //falling
+           marioObj.y += 5; 
+           jumpUpOrDown = "Down";
+        } else if (marioObj.y >= marioObj.constantFloor) {         // set mario on floor when falling
+            marioObj.y = marioObj.constantFloor;
+            marioObj.onGround = true; 
+            jumpUpOrDown = "Up";
+        }       
 }
+
+
+// BricksCoin , kasichki, trabnaPregrada, talbi, vragove
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+  
+
+ let cointMoveUp = 340; 
+ let marioHit = false;
+ function targetBrickCoin(bricksX, bricksLength, setJumpLimit) {
+    if (globalPosition > bricksX && globalPosition < bricksLength + bricksX) {
+        marioObj.jumpLimit = setJumpLimit;
+        if (globalPosition >=  bricksX && globalPosition < bricksLength + bricksX) {
+            ctx.save();
+            if (marioObj.y >= 400 && marioObj.y < 410) {
+                marioHit = true; 
+            }
+            if ( marioHit == true && cointMoveUp > 280) {
+                ctx.font = "20px sanserif";
+                ctx.fillText("COIN", marioObj.x , cointMoveUp);
+                cointMoveUp -= 1;
+            }            
+            ctx.restore();
+        }
+    } else {
+        marioObj.jumpLimit = 220;
+        cointMoveUp = 340;
+        marioHit = false;
+    }
+}
+
+function jbricksObjectes() {
+    if (globalPosition > 0 && globalPosition <= 630)
+        targetBrickCoin(600, 30, 370);
+    if (globalPosition > 630 && globalPosition <= 710)
+        targetBrickCoin(690,20,370);
+    if (globalPosition > 710 && globalPosition <= 750) {
+        targetBrickCoin(720,20,370);
+
+        // if() rof second floor
+    }    
+    
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 function draw() { 
     ctx.beginPath();
     ctx.clearRect(0,0,1200,600);
-    ctx.translate(0,0);
+    //ctx.translate(0,0);
     drawBackground(marioObj.translateX, marioObj.translateY);
-    marioStepsAndRotate(marioObj.scaleRight, marioObj.x, marioObj.y);
+    marioStepsAndRotate();
+    marioPhysics();
+    ctx.fillText("globalPosition:" + globalPosition, 70,50);
+    ctx.fillText("MarioObj.x : " + marioObj.x, 70, 80);
+    ctx.fillText("MarioObj.translateX : " + marioObj.x, 70, 110);
+    jbricksObjectes();
+
     ctx.closePath();
 }
-
 //Main Game Loop
 let loadingCounter = 0;
 
-function mainGameLoop() {  
-    clearInterval(startLoading);
-    if (isLoading == false && loadingCounter < 2000) {
-        draw();
-        ctx.font = "45px monospace";
-        ctx.fillStyle = "white";
-        ctx.fillText("You can Start to Play",80,250); 
-        ctx.font ="30px monospace";
-        ctx.fillText("Use Left, Right Arrow and Space.",80,300);  
-        loadingCounter++;
-
-        if (globalPosition > 405) {
-            loadingCounter = 2000;
-        }
-    } else {
-        draw();
-    }   
+function mainGameLoop() {    
     updateContlos();
-    audio.play();
+    draw();
+    if (isLoading == false && loadingCounter < 2000) {
+        controlsInstructions();
+        clearInterval(startLoading);
+    }  
+    audio.play(); //for mario music
+    //second part 
     
+
+
     //requestAnimationFrame(mainGameLoop);  
 }
 //mainGameLoop();
 
 let startLoading = setInterval(drawAnimationLoading, 50);
 setTimeout(function() {setInterval(mainGameLoop, 10);}, 5000);
-
-
-
 
 
 
